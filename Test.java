e47092d670dd089ebe0feb4bde8a3799bed5668b
Sarkararup <arup252003@gmail.com>
2026-07-01 09:08:09 +0530

    Test.java updated

--- Test.java
+++ Test.java
@@ -1,5 +1,5 @@
-// public Test{
-//     public static void main(String[] args) {
-//         System.out.println("My name is Arup");
-//     }
-// }
+public class Test{
+    public static void main(String[] args) {
+        System.out.println("Hy Arup is Here");
+    }
+}
